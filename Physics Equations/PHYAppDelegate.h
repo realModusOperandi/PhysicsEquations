00653105ed661ebf0a11b1486cce645f015cfd0d
Liam Westby <lw322@nau.edu>
2014-01-21 23:16:35 -0700

    Initial

--- Physics Equations/PHYAppDelegate.h
+++ Physics Equations/PHYAppDelegate.h
@@ -9,7 +9,14 @@
 #import <Cocoa/Cocoa.h>
 
 @interface PHYAppDelegate : NSObject <NSApplicationDelegate>
+- (IBAction)calculateForce:(id)sender;
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *charge1;
+@property (weak) IBOutlet NSTextField *charge1exp;
+@property (weak) IBOutlet NSTextField *charge2;
+@property (weak) IBOutlet NSTextField *charge2mag;
+@property (weak) IBOutlet NSTextField *distance;
+@property (weak) IBOutlet NSTextField *force;
 
 @end
--- Physics Equations/PHYAppDelegate.m
+++ Physics Equations/PHYAppDelegate.m
@@ -15,4 +15,18 @@
     // Insert code here to initialize your application
 }
 
+- (IBAction)calculateForce:(id)sender {
+    NSNumber *charge1value = [NSNumber numberWithDouble:[[self charge1] doubleValue]*pow(10,[[self charge1exp] doubleValue])];
+    //NSLog(@"%f", [[self charge1] doubleValue]*pow(10,[[self charge1exp] doubleValue]));
+    NSNumber *charge2value = [NSNumber numberWithDouble:[[self charge2] doubleValue]*pow(10,[[self charge2mag] doubleValue])];
+    NSNumber *constantValue = [NSNumber numberWithDouble:9.0 * pow(10, 9)];
+    NSNumber *squareDistance = [NSNumber numberWithDouble:pow([[self distance] doubleValue], 2)];
+    double forceValue = [constantValue doubleValue] * ([charge1value doubleValue] * [charge2value doubleValue]) / [squareDistance doubleValue];
+    
+    [[self force] setStringValue:[NSString stringWithFormat:@"%g Nm^2/C^2", forceValue]];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
 @end
